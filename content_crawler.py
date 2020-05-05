c283d17783898ff187efad0aed6d868ac58cfad9
Jim-LSJ <jim861026@gmail.com>
2020-05-05 15:12:42 +0800

    revise exception

--- content_crawler.py
+++ content_crawler.py
@@ -2,13 +2,15 @@ import csv, os
 from urllib.request import urlopen
 from urllib import error
 from selenium import webdriver
+from selenium import common
+import time
 
 def read_file(filename):
     file = open(filename, newline='')
     rows = csv.reader(file)
     return rows
 
-def crawl_url(filename = "20200415-28.csv"):
+def crawl_url():
     uuid_file = open('uuid.csv', 'w+', newline='')
     uuid_file = csv.writer(uuid_file)
 
@@ -19,45 +21,52 @@ def crawl_url(filename = "20200415-28.csv"):
 
     based_url = "https://urlscan.io/result/"
 
-    data = read_file(filename)
+    data = read_file("old_data.csv")
     for idx, row in enumerate(data):
         if idx == 0:    # first row if info
             continue
         
-        ##########################
+        #####################
         # control
-        ##########################
-        if idx < 1516:
+        #####################
+        if idx <= 100000:
             continue
-        if idx > 80000:
+        if idx > 200000:
             break
-        ##########################
+        #####################
 
         url = based_url + row[2] + "/dom/"
 
         try:
             driver.get(url)
             code = driver.find_element_by_tag_name('code')
-            print('{} find code'.format(url))
-        except:
+        except common.exceptions.TimeoutException:
+            print('{} Timeout'.format(url))
+            continue
+        except common.exceptions.NoSuchElementException:
             print('{} cannot find code'.format(url))
             continue
 
+        ####################################
         # wait for dynamically loading html
-        counter = 0
+        ####################################
+        tic = time.clock()
         flag = 0
         while not code.text:
-            if counter > 100:
+            toc = time.clock()
+            if toc - tic > 10:
                 flag = 1
                 break
-            counter += 1
         if flag:
+            print('{} js loading fail'.format(url))
             continue
+        ####################################
 
         uuid_file.writerow([row[2]])
         output_file = open(os.path.join('html_files', row[2] + '.html'), 'w')
         output_file.write(code.text)    # cp950 error under windows OS
         output_file.close()
+        print('{} find code'.format(url))
 
     uuid_file.close()
     return
@@ -65,7 +74,7 @@ def crawl_url(filename = "20200415-28.csv"):
 def main():
     if 'html_files' not in os.listdir('.'):
         os.mkdir('html_files')
-    crawl_url("20200415-28.csv")
+    crawl_url()
 
 if __name__ == '__main__':
     main()
